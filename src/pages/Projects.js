2ec3f161819c5a45d9a6716a96c54ba82d300e93
jisoooo17 <jisoooo17@gmail.com>
2024-08-07 13:16:06 +0900

    프로젝트 페이지 스와이퍼 작업 시작

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 const Projects = () => {
   return (
@@ -9,7 +18,28 @@ const Projects = () => {
       <Header/>
 
       <section className="project">
-
+        .tit-area
+        <div className="inner">
+          <div className="tit-area">
+            
+          </div>
+          <div className="cont-area">
+          <Swiper
+              // install Swiper modules
+              modules={[Navigation, Pagination, Scrollbar]}
+              slidesPerView={1}
+              navigation
+              pagination={{ clickable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log('slide change')}
+            >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+          </Swiper>
+          </div>
+        </div>
       </section>
 
       <Footer/>
