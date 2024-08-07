fb57c82daa7299fec020bf26788c1852b719b624
jisoooo17 <jisoooo17@gmail.com>
2024-08-07 12:23:10 +0900

    클래스명 수정

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -17,8 +17,8 @@ const Intro = () => {
   useEffect(() => {
     setTimeout(() => {
       const welcomeArea = document.querySelector('.welcome-area');
-      const titArea = document.querySelector('.tit_area');
-      const imgWrap = document.querySelector('.img_wrap');
+      const titArea = document.querySelector('.tit-w');
+      const imgWrap = document.querySelector('.img');
   
       if (welcomeArea) {
         welcomeArea.style.transition = 'opacity 0.3s ease-out';
@@ -40,16 +40,16 @@ const Intro = () => {
         <Lottie animationData={welcome} autoplay style={{ height: '700px', width: '700px',position:"absolute", top:"50%", left:"50%", zIndex:1, transform:"translate(-50%, -50%)"}}  lottieRef={lottieRef} />
       </div>
 
-      <div className="cont-area">
-        <div className="tit_area">
+      <div className="content-area">
+        <div className="tit-w">
           <p className="tit"><span>신입 프론트엔드 개발자 김지수입니다.</span></p>
           <div className="made">
             <button type="button" onClick={()=>{navigate("/aboutme")}}><span>ABOUT ME</span></button>
             <button type="button" onClick={()=>{navigate("/projects")}}><span>PROJECTS</span></button>
           </div>      
         </div>
-        <div className="img_area">
-          <div className="img_wrap scroll_motion">
+        <div className="img-w">
+          <div className="img scroll_motion">
             <img src={process.env.PUBLIC_URL + 'img-project.jpg'} alt="" />
           </div>
         </div>
